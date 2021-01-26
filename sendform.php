54b8aa02feb0b43764a79973fa13f1b84b9ececf
Andrey <lagunmelman@gmail.com>
2021-01-26 23:44:46 +0300

    Форма отправуи в телегу

---
+++ sendform.php
@@ -0,0 +1,25 @@
+<?php
+//Сбор данных из полей формы. 
+$name = $_POST['name'];// Берём данные из input c атрибутом name="name"
+$phone = $_POST['phone']; // Берём данные из input c атрибутом name="phone"
+$email = $_POST['email']; // Берём данные из input c атрибутом name="mail"
+
+$token = "1660441980:AAEVzhjSZscKURkS3w8AcI7fwYiBjFsUmT0"; // Тут пишем токен
+$chat_id = "-502718219"; // Тут пишем ID группы, куда будут отправляться сообщения
+$sitename = "molocity"; //Указываем название сайта
+
+$arr = array(
+
+  'Заказ с сайта: ' => $sitename,
+  'Имя: ' => $name,
+  'Телефон: ' => $phone,
+  'Сообщение' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+?>
